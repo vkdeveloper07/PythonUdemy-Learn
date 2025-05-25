2c033b61064f04157bc3038bc959a8ab54fb083d
Vishal Kharade <vishalthedeveloper07@gmail.com>
2025-05-25 11:25:46 +0530

    Python day 1 udemy section 1

---
+++ 11-BrandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Brand Name Generator!")
+# Ask the user for their city and pet name
+city = input("What is the name of your city? \n")
+pet = input("What is the name of your pet? \n")
+print("Your brand name could be: " + city + " " + pet)
---
+++ 7.string-manipulation.py
@@ -0,0 +1,2 @@
+print("Hello python\nHello python1\nHello python2"); #new line
+print("Hello " + "python") #concatenation
